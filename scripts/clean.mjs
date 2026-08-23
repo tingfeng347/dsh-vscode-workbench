eed871ef53e1b547e9a5b15f8ac1ca7feff2737a
qiaojingbo <3152912378@qq.com>
2026-08-23 23:22:17 +0800

    chore: 初始化工作台工程配置

---
+++ scripts/clean.mjs
@@ -0,0 +1,2 @@
+import { rm } from 'node:fs/promises'
+for (const path of process.argv.slice(2)) await rm(path, { recursive: true, force: true })
---
+++ tsdown.config.ts
@@ -0,0 +1,2 @@
+import { dshPackage } from './build/client-bundle.ts'
+export default dshPackage('dsh-vscode-workbench')
